41f2f9376e147814b6495fd61400e5a034d8001c
AlexAznar <alejandro.aznar@tuguu.com>
2014-06-09 20:52:15 +0100

    - create a new block for report pannel

--- src/Contagric/BackendBundle/Block/StatsBlockService.php
+++ src/Contagric/BackendBundle/Block/StatsBlockService.php
@@ -2,48 +2,50 @@
 
 namespace Contagric\BackendBundle\Block;
 
-
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 use Sonata\BlockBundle\Model\BlockInterface;
 use Sonata\BlockBundle\Block\BlockContextInterface;
+use Sonata\BlockBundle\Block\BaseBlockService;
 
 use Sonata\AdminBundle\Form\FormMapper;
 use Sonata\AdminBundle\Validator\ErrorElement;
+
 
 class StatsBlockService extends BaseBlockService
 {
     public function getName()
     {
-        return 'Mis consultas';
+        return 'Mis estadisticas!';
     }
 
-    public function getDefaultSettings(OptionsResolverInterface $resolver)
+    public function setDefaultSettings(OptionsResolverInterface $resolver)
     {
-            $resolver->setDefaults(array(
-                'url'      => false,
-                'title'    => 'Consultas',
-                'template' => 'BackendBundle:Block:stats_dashboard_block.html.twig',
-            ));
+        $resolver->setDefaults(array(
+            'url'      => false,
+            'title'    => 'Consultas y Reportes',
+            'template' => 'BackendBundle:Block:stats_dashboard_block.html.twig',
+        ));
     }
 
-    public function validateBlock(ErrorElement $errorElement, BlockInterface $block)
+    public function execute(BlockContextInterface $blockContext, Response $response = null)
     {
+        $settings = $blockContext->getSettings();
+
+        return $this->renderResponse($blockContext->getTemplate(), array(
+            'block'     => $blockContext->getBlock(),
+            'settings' => $settings,
+            ), $response);
     }
 
     public function buildEditForm(FormMapper $formMapper, BlockInterface $block)
     {
+        throw new \Exception();
     }
 
-    public function execute(BlockInterface $block, Response $response = null)
+    public function validateBlock(ErrorElement $errorElement, BlockInterface $block)
     {
-        // merge settings
-        $settings = $block->getSettings();
-
-        return $this->renderResponse('BackendBundle:Block:stats_dashboard_block.html.twig', array(
-            'block'     => $block->getBlock(),
-            'settings'  => $settings
-            ), $response);
+        throw new \Exception();
     }
 }
